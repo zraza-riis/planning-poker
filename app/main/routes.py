40e98d8f02e83f509e3d625ef58a43d976517a47
zraza-riis <zraza@riis.com>
2024-02-17 12:26:45 -0500

    create room backend

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template, jsonify, url_for, request
 from app.main import bp
+import uuid
+
+from app import db
+from app.main import bp
+from app.models import Room
 
 @bp.route('/')
 def index():
@@ -8,3 +13,22 @@ def index():
 @bp.route('/about')
 def about():
     return render_template('about.html')
+
+@bp.route('/create_room', methods=['POST'])
+def create_room():
+    room_uuid = str(uuid.uuid4())
+    room_link = url_for('main.join_room', room_uuid=room_uuid, _external=True)
+    room_name = request.form.get('room_name')
+    room_description = request.form.get('room_description')
+
+    room = Room(name=room_name, description=room_description, uuid=room_uuid)
+    db.session.add(room)
+    db.session.commit()
+
+    room_link = url_for('auth.join_room', room_uuid=room_uuid, _external=True)
+
+    return jsonify({'room_uuid': room_uuid, 'room_link': room_link})
+
+
+def join_room(room_uuid):
+    return render_template("join_room.html", room_uuid=room_uuid)
---
+++ app/models/room.py
@@ -0,0 +1,13 @@
+from app.extensions import db
+from datetime import datetime
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
+    uuid = db.Column(db.String(36), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Room {self.name}>'
+
