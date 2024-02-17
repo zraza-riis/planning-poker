a192794c682be946b0bed2f0c9e053dfac369dcf
zraza-riis <zraza@riis.com>
2024-02-17 12:33:48 -0500

    fix

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,7 +14,7 @@ def index():
 def about():
     return render_template('about.html')
 
-@bp.route('/create_room', methods=['POST'])
+@bp.route('/create-room', methods=['POST'])
 def create_room():
     room_uuid = str(uuid.uuid4())
     room_link = url_for('main.join_room', room_uuid=room_uuid, _external=True)
