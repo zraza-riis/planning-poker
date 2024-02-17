d51a0fe8d1b8118d5cb9e8697bd8ee9d5d647b33
zraza-riis <zraza@riis.com>
2024-02-17 15:35:18 -0500

    players can share a room

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,7 @@ import uuid
 
 from app import db
 from app.main import bp
-from app.models import Room
+from app.models import Room, Player
 
 @bp.route('/')
 def index():
@@ -20,11 +20,13 @@ def create_room():
         room_uuid = str(uuid.uuid4())
         room_name = request.form.get('room_name')
         room_description = request.form.get('room_description')
+        join_link = url_for('main.join_room', room_uuid=room_uuid, _external=True)
 
-        room = Room(name=room_name, description=room_description, uuid=room_uuid)
+        room = Room(name=room_name, description=room_description, uuid=room_uuid, join_link=join_link)
         db.session.add(room)
         db.session.commit()
-        return redirect(url_for('main.join_room', room_uuid=room_uuid))
+
+        return redirect(join_link)
     return render_template('create_room.html')
 
 @bp.route('/join/<room_uuid>', methods=['GET', 'POST'])
@@ -32,6 +34,11 @@ def join_room(room_uuid):
     room = Room.query.filter_by(uuid=room_uuid).first_or_404()
     if request.method == 'POST':
         display_name = request.form.get('display_name')
+
+        player = Player(name=display_name, room=room)
+        db.session.add(player)
+        db.session.commit()
+
         return redirect(url_for('main.room_page', room_uuid=room_uuid))
     return render_template("join_room.html", room=room)
 
--- app/models/room.py
+++ app/models/room.py
@@ -6,6 +6,7 @@ class Room(db.Model):
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200), nullable=True)
     uuid = db.Column(db.String(36), unique=True, nullable=False)
+    join_link = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
